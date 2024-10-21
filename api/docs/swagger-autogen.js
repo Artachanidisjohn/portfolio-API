eb2b04a15a02eb19e82c28d598e985ae384a77ba
Artachanidis Ioannis <82318135+Artachanidisjohn@users.noreply.github.com>
2024-10-21 11:33:40 +0300

    Delete api/docs directory

--- api/docs/swagger-autogen.js
+++
@@ -1,9 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')();
-const outputFile = './swagger_output.json';
-
-//ΒΑΖΩ ΕΔΩ ΤΑ FILES ΑΠΟ ΤΑ ROUTES ΑΠΟ ΟΠΟΥ ΘΕΛΩ ΝΑ ΔΗΜΙΟΥΡΓΗΣΕΙ ΤΟ JSON
-const endpointsFiles = ['../routes/recordRoutes.js'];
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    console.log('Swagger documentation generated');
-});
